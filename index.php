7ad2d8591fe353ecbe166242c80ff793959aad50
JoshuaDuyPham <joshuadpham@gmail.com>
2017-03-29 15:10:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,4 @@
 <?php
-$dsn = "pgsql:"
-    . "ec2-50-17-236-15.compute-1.amazonaws.com;"
-    . "d2641hluvfmus5;"
-    . "user=nqxatreczgovme;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=91ef1bd79a01039737b248bdc7a465b27b284af93a537cfd217a5135d94f95d8";
-$db = new PDO($dsn);
-?>
-
-<html>
- <head>
-  <title>Employees</title>
- </head>
- <body>
-  <table>
-   <thead>
-    <tr>
-     <th>Employee ID</th>
-     <th>Last Name</th>
-     <th>First Name</th>
-    </tr>
-   </thead>
-   <tbody>
-<?php
-$query = "SELECT ssn, lname, fname, title "
-     . "FROM employees ORDER BY lname ASC, fname ASC";
-$result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>" . $row["ssn"] . "</td>";
-    echo "<td>" . htmlspecialchars($row["lname"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["fname"]) . "</td>";
-    echo "</tr>";
-}
-$result->closeCursor();
-?>
-   </tbody>
-  </table>
- </body>
-</html>
-/*
-<?php
   ob_start();
   session_start();
            
@@ -111,4 +68,3 @@ $result->closeCursor();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </body>
 </html>
-*/
