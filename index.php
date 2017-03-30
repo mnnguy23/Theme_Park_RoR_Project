cd7194ccef6471477ae7d0b54757333fc952a115
mnnguy23 <michaelnguyen1105@gmail.com>
2017-03-30 08:47:24 -0500

    added links for development and deployment on mnnguy23 personal machine.

--- index.php
+++ index.php
@@ -22,24 +22,29 @@ $twig = new Twig_Environment($loader, array(
 ?>
 <?php
 $template = $twig->load('index.html');
+// development link
+//$clearSession = "http://ta_code.dev/logout.php"
+// deployment link
+$clearSession = "https://theme-park-management.herokuapp.com/logout.php";
+
 $username = 'tutorialspoint';
 $password = '1234';
 $msg = '';
       
-        if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password']))  {
-        
-           // This is currently hard coded and needs to be changed to pull actual information from the database.
-          
-          if ($_POST['username'] == $username && $_POST['password'] == $password) {
-            $_SESSION['valid'] = true;
-            $_SESSION['timeout'] = time();
-            $msg = 'You have entered a valid user name and password!';
-          } else {
-            $msg = 'Wrong username and password';
-          }
-        } else {
-          $msg = 'Empty Username and Password';
-        }
+  if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password']))  {
+  
+     // This is currently hard coded and needs to be changed to pull actual information from the database.
+    
+    if ($_POST['username'] == $username && $_POST['password'] == $password) {
+      $_SESSION['valid'] = true;
+      $_SESSION['timeout'] = time();
+      $msg = 'You have entered a valid user name and password!';
+    } else {
+      $msg = 'Wrong username and password';
+    }
+  } else {
+    $msg = 'Empty Username and Password';
+  }
 echo $template->render(array('msg' => $msg));
 
 ?>
--- logout.php
+++ logout.php
@@ -6,15 +6,20 @@ require_once 'vendor/autoload.php';
    unset($_SESSION["login"]);
 ?>
 <?php
-   $loader = new Twig_Loader_Filesystem('templates');
-   $twig = new Twig_Environment($loader, array(
-     'auto_reload' => true
-   ));
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+ 'auto_reload' => true
+));
 ?>
 
 <?php
-  $template = $twig->load('logout.html');
-  $msg = 'You have cleaned session';
-  $loginLink = "https://theme-park-management.herokuapp.com/";
-  echo $template->render(array('msg' => $msg, 'link' => $loginLink));
+$template = $twig->load('logout.html');
+$msg = 'You have cleaned session';
+
+// dev link
+//$loginLink = "http://ta_code.dev/index.php";
+
+// deployment link
+$loginLink = "https://theme-park-management.herokuapp.com/index.php"
+echo $template->render(array('msg' => $msg, 'link' => $loginLink));
 ?>
