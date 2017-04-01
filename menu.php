eba27c322b72997d9e25b1b7fca3d012c2054fa2
mnnguy23 <michaelnguyen1105@gmail.com>
2017-03-31 22:47:50 -0500

    Added menu/dashboard.

---
+++ menu.php
@@ -0,0 +1,10 @@
+<?php
+include 'app/base.php';
+$twig = loadEnvironment();
+  $loginLink = "http://ta_code.dev/index.php";
+?>
+
+<?php
+  $template = $twig->load('menu.html');
+  echo $template->render(array('login' => $loginLink));
+?>
