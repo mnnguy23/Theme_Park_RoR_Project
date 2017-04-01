553f66e5938c17be2b85e961584f110d81d84789
JoshuaDuyPham <joshuadpham@gmail.com>
2017-03-31 23:15:03 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -8,3 +8,43 @@ $twig = loadEnvironment();
   $template = $twig->load('menu.html');
   echo $template->render(array('login' => $loginLink));
 ?>
+
+<html>
+
+  <head>
+   <title>Database</title>
+  </head>
+
+  <body bgcolor="white">
+
+  <?
+    $isDevelopment = false;
+    $dbConn = loadDB($isDevelopment);
+    $result = pg_query($dbConn, "select name,ride_id from ride");
+    $numrows = pg_numrows($result);
+  ?>
+
+  <table border="1">
+  <tr>
+   <th>Name</th>
+   <th>ID</th>
+  </tr>
+  <?
+
+   // Loop on rows in the result set.
+
+   for($ri = 0; $ri < $numrows; $ri++) {
+    echo "<tr>\n";
+    $row = pg_fetch_array($result, $ri);
+    echo " <td>", $row["name"], "</td>
+   <td>", $row["ride_id"], "</td>
+  </tr>
+  ";
+   }
+   pg_close($link);
+  ?>
+  </table>
+
+  </body>
+
+  </html>
