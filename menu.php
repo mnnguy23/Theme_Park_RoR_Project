52acfb57e71c2d5c2b70fb3828a4ded7b1fc990e
JoshuaDuyPham <joshuadpham@gmail.com>
2017-04-01 00:15:45 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,49 +2,48 @@
   include 'app/base.php';
   $twig = loadEnvironment();
   $loginLink = "http://ta_code.dev/index.php";
-  $isDevelopment = false;
-  $db = loadDB($isDevelopment);
-  $result = pg_query($db, "SELECT ride_id,name, FROM ride");
-  $numrows = pg_numrows($result);
 ?>
 
 <?php
   $template = $twig->load('menu.html');
   echo $template->render(array('login' => $loginLink)); 
 ?>
-
 <html>
- <head>
-  <title>Attractions</title>
- </head>
- <body>
-  <table>
-   <thead>
-    <table border="1">
-    <tr>
-      <th>Name</th>
-      <th>ID</th>
-    </tr>
+
+  <head>
+   <title>Database</title>
+  </head>
+
+  <body bgcolor="white">
+
   <?
-   </thead>
-   <tbody>
-  <?php
-$query = "SELECT ride_id, name, dno"
-     . "FROM employee ORDER BY name ASC";
-$result = $db->query($query);
-// Loop on rows in the result set.
+  $isDevelopment = false;
+  $db = loadDB($isDevelopment);
+  $result = pg_query($db, "SELECT ride_id,name, FROM ride");
+  $numrows = pg_numrows($result);
+  ?>
+
+  <table border="1">
+  <tr>
+   <th>Name</th>
+   <th>ID</th>
+  </tr>
+  <?
+
+   // Loop on rows in the result set.
 
    for($ri = 0; $ri < $numrows; $ri++) {
     echo "<tr>\n";
     $row = pg_fetch_array($result, $ri);
-    echo " <td>", $row["name"], "</td>
-   <td>", $row["ride_id"], "</td>
+    echo " <td>", $row["ride_id"], "</td>
+    <td>", $row["name"], "</td>
   </tr>
   ";
    }
-$result->closeCursor();
-?>
-   </tbody>
+   pg_close($link);
+  ?>
   </table>
- </body>
-</html>
+
+  </body>
+
+  </html>
