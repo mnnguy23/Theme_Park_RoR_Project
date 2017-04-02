0ea6bc035b94a23a8472e529c1ab58705ec6fb50
JoshuaDuyPham <joshuadpham@gmail.com>
2017-04-02 18:57:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,26 +20,3 @@
     return pg_query($dbConn, $sql);
   }
 ?>
-
-<html>
-  <body bgcolor="white">
-    <table border="1">
-    <tr>
-      <th>Ride ID</th>
-      <th>Name</th>
-    </tr>
-  <?
-   // Loop on rows in the result set.
-   for($ri = 0; $ri < $numrows; $ri++) {
-    echo "<tr>\n";
-    $row = pg_fetch_array($result, $ri);
-    echo " <td>", $row["ride_id"], "</td>
-   <td>", $row["name"], "</td>
-  </tr>
-  ";
-   }
-   pg_close($db);
-  ?>
-  </table>
-  </body>
-</html>
