6f996085e9f25b5b408ed2a284491d65f1d005c8
JoshuaDuyPham <joshuadpham@gmail.com>
2017-04-02 18:54:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,31 @@
 ?>
 
 <?php
-function pg_list_tables($dbConn) {
-  $sql = "select relname from pg_stat_user_tables order by relname;";
-  return pg_query($dbConn, $sql);
-}
+  function pg_list_tables($dbConn) {
+    $sql = "select relname from pg_stat_user_tables order by relname;";
+    return pg_query($dbConn, $sql);
+  }
 ?>
+
+<html>
+  <body bgcolor="white">
+    <table border="1">
+    <tr>
+      <th>Ride ID</th>
+      <th>Name</th>
+    </tr>
+  <?
+   // Loop on rows in the result set.
+   for($ri = 0; $ri < $numrows; $ri++) {
+    echo "<tr>\n";
+    $row = pg_fetch_array($result, $ri);
+    echo " <td>", $row["ride_id"], "</td>
+   <td>", $row["name"], "</td>
+  </tr>
+  ";
+   }
+   pg_close($db);
+  ?>
+  </table>
+  </body>
+</html>
