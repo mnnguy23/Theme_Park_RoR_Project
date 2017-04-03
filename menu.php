fb2f557fafa4ab5ffd8646f4145f84e09e1a37e7
JoshuaDuyPham <joshuadpham@gmail.com>
2017-04-03 17:12:57 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php
   include 'app/base.php';
   $twig = loadEnvironment();
-  $loginLink = "https://theme-park-management.herokuapp.com/addEmployee.php";
+  $loginLink = "https://theme-park-management.herokuapp.com/index.php";
 ?>
 
 <?php
