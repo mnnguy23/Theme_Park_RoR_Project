b6321795ff0f670df5d2740534925145fd4f7985
mnnguy23 <michaelnguyen1105@gmail.com>
2017-04-04 17:14:57 -0500

    fixing errors with the login.

--- index.php
+++ index.php
@@ -9,18 +9,11 @@
 
 <?php
   $template = $twig->load('index.html');
-  $data = queryUserAccess($dbConn, $isDevelopment);
-  list($valid, $result) = checkCredential($data);
+  //$data = queryUserAccess($dbConn, $isDevelopment);
+  //list($valid, $result) = checkCredential($data);
   
   echo $template->render(array('msg' => $result, 'clear' => $clearSession));
     if($valid) {
       menuRedirect();
     }
-?>
-
-<?php
-function pg_list_tables($dbConn) {
-  $sql = "select relname from pg_stat_user_tables order by relname;";
-  return pg_query($dbConn, $sql);
-}
 ?>
