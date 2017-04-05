61bd84f42ce0a889ccfe9618637b6db519b47c58
mnnguy23 <michaelnguyen1105@gmail.com>
2017-04-05 17:32:34 -0500

    Changed display employees.

--- displayEmp.php
+++ displayEmp.php
@@ -8,30 +8,23 @@ $dbConn = loadDB($isDevelopment);
 ?>
 
 <?php
-  $employee = displayEmployees($dbConn, $isDevelopment);
+  $employee = displayEmployees($dbConn);
   
   $template = $twig->load('displayEmp.html');
-  $params = array('logout' => $clearSession, 'user' => $user, 'name' => $name, 'emplyee' => $employee);
+  $params = array('logout' => $clearSession, 'user' => $user, 'name' => $name, 'employees' => $employee);
   echo $template->render($params);
 ?>
 
 <?php
-function displayEmployees($db, $isDevelopment) {
+function displayEmployees($db) {
   $data = array();
-  if($isDevelopment) {
-    $results = pg_query($db ,"SELECT e_name, employee_id, bdate, address, phone_number, salary, dno  FROM public.attraction");
-    while($row = pg_fetch_row($results)) {
-      $data[] = array($row["name"], $row["employee_id"], $row["bdate"], row["address"], row["phone_number"], row["salary"], row["dno"]);
-    }
-  } else {
     $query = "SELECT e_name, employee_id, bdate, address, phone_number, salary, dno"
          . " FROM employee";
     $result = $db->query($query);
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      $data[] = array($row["e_name"], $row["employee_id"], $row["bdate"], row["address"], row["phone_number"], row["salary"], row["dno"]);
+      $data[] = array($row["e_name"], $row["employee_id"], $row["bdate"], $row["address"], $row["phone_number"], $row["salary"], $row["dno"]);
     }
     $result->closeCursor();
-  }
   return $data;
 }
 ?>
