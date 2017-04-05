649abf37a8e551be374886322e7c5211298f9376
mnnguy23 <michaelnguyen1105@gmail.com>
2017-04-05 13:41:18 -0500

    Fixed the maintenance report to show employee name.

--- menu.php
+++ menu.php
@@ -21,7 +21,7 @@ $dbConn = loadDB($isDevelopment);
 function maintenanceReport($db, $isDevelopment) {
   $data = array();
   if($isDevelopment) {
-    $results = pg_query($db ,"select E.name, A.name, M.maintenance_date, M.maintenance_cost, A.operational from public.employee as E, public.attraction as A, public.attraction_maintenance as M where M.e_id=E.employee_id and M.am_id=A.attraction_id;");
+    $results = pg_query($db ,"select E.e_name, A.name, M.maintenance_date, M.maintenance_cost, A.operational from public.employee as E, public.attraction as A, public.attraction_maintenance as M where M.e_id=E.employee_id and M.am_id=A.attraction_id;");
     while($row = pg_fetch_row($results)) {
       $empName = $row[0];
       $attractName = $row[1];
@@ -35,7 +35,7 @@ function maintenanceReport($db, $isDevelopment) {
       $data[] = array($empName, $attractName, $maintDate, $cost, $isOperational);
     }
   } else {
-    $query = "SELECT E.employee_id, A.name, M.maintenance_date, M.maintenance_cost, A.operational FROM employee as E, attraction as A, attraction_maintenance as M WHERE M.e_id=E.employee_id and M.am_id=A.attraction_id;";
+    $query = "SELECT E.e_name, A.name, M.maintenance_date, M.maintenance_cost, A.operational FROM employee as E, attraction as A, attraction_maintenance as M WHERE M.e_id=E.employee_id and M.am_id=A.attraction_id;";
     $result = $db->query($query);
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
       if($row['operational'] == 't') {
@@ -43,7 +43,7 @@ function maintenanceReport($db, $isDevelopment) {
       } else {
         $isOperational = "No";
       }
-      $data[] = array($row["employee_id"], $row["name"], $row["maintenance_date"],$row["maintenance_cost"],  $isOperational);
+      $data[] = array($row["e_name"], $row["name"], $row["maintenance_date"],$row["maintenance_cost"],  $isOperational);
     }
     $result->closeCursor();
   }  
