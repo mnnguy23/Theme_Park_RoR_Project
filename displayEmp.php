2e7f9912f078a06413bd23dc2469733d62e770ed
JoshuaDuyPham <joshuadpham@gmail.com>
2017-04-05 11:43:25 -0500

    Update displayEmp.php

--- displayEmp.php
+++ displayEmp.php
@@ -1,32 +1,30 @@
 <?php
-session_start();
-$dsn = "pgsql:"
-    . "host=ec2-50-17-236-15.compute-1.amazonaws.com;"
-    . "dbname=d2641hluvfmus5;"
-    . "user=nqxatreczgovme;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=91ef1bd79a01039737b248bdc7a465b27b284af93a537cfd217a5135d94f95d8";
-$db = new PDO($dsn);
-  ob_start();
-  session_start();
-           
+include 'app/base.php';
+include 'app/indexFunctions.php';
+$isDevelopment = false;
+$twig = loadEnvironment();
+$clearSession = developmentMode($isDevelopment);
+$dbConn = loadDB($isDevelopment);
 ?>
 
 <?php
-  include 'app/base.php';
-  include 'app/indexFunctions.php';
-  $twig = loadEnvironment();
-  ?>
+  $rides = displayEmployees($dbConn, $isDevelopment);
   
+  $template = $twig->load('displayEmp.html');
+  $params = array('logout' => $clearSession, 'user' => $user, 'name' => $name, 'rides' => $rides);
+  echo $template->render($params);
+?>
+
 <?php
-	$template = $twig->load('displayEmp.html');
-	
-	$query = "SELECT fname, lname, ssn, bdate, address, salary, dno, phone_number"
-			"FROM employee";
-	$result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      $data[] = array($row["fname"], $row["lname"], $row["snn"], $row["salary"]);
+function displayEmployees($db, $isDevelopment) {
+  $data = array();
+    $query = "SELECT name, employee_id, bdate, address, phone_number, salary, dno"
+         . " FROM employee";
+    $result = $db->query($query);
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      $data[] = array($row["name"], $row["employee_id"], $row["bdate"], row["address"], row["phone_number"], row["salary"], row["dno"]);
     }
-	echo $template->render(array('msg' => $msg, 'clear' => $clearSession, 'empData' => $data));
+    $result->closeCursor();
+  return $data;
+}
 ?>
