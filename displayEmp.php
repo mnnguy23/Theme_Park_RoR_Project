44cd46ff84f1b9e024af5138c5d6005c95eb2922
JoshuaDuyPham <joshuadpham@gmail.com>
2017-04-05 12:01:22 -0500

    Update displayEmp.php

--- displayEmp.php
+++ displayEmp.php
@@ -11,13 +11,19 @@ $dbConn = loadDB($isDevelopment);
   $rides = displayEmployees($dbConn, $isDevelopment);
   
   $template = $twig->load('displayEmp.html');
-  $params = array('logout' => $clearSession, 'user' => $user, 'name' => $name, 'rides' => $rides);
+  $params = array('logout' => $clearSession, 'user' => $user, 'name' => $name);
   echo $template->render($params);
 ?>
 
 <?php
 function displayEmployees($db, $isDevelopment) {
   $data = array();
+  if($isDevelopment) {
+    $results = pg_query($db ,"SELECT name, employee_id, bdate, address, phone_number, salary, dno  FROM public.attraction");
+    while($row = pg_fetch_row($results)) {
+      $data[] = array($row["name"], $row["employee_id"], $row["bdate"], row["address"], row["phone_number"], row["salary"], row["dno"]);
+    }
+  } else {
     $query = "SELECT name, employee_id, bdate, address, phone_number, salary, dno"
          . " FROM employee";
     $result = $db->query($query);
@@ -25,6 +31,7 @@ function displayEmployees($db, $isDevelopment) {
       $data[] = array($row["name"], $row["employee_id"], $row["bdate"], row["address"], row["phone_number"], row["salary"], row["dno"]);
     }
     $result->closeCursor();
+  }
   return $data;
 }
 ?>
