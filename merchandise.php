36983a2a4183518c89abc6d7ede59fd7f2784a3e
jperez0198 <jperez0198@yahoo.com>
2017-04-05 16:37:32 -0500

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -1,6 +1,5 @@
 <?php
 	include 'app/base.php';
-	include 'app/indexFunctions.php';
 	$isDevelopment = false;
 	$twig = loadEnvironment();
 	$clearSession = developmentMode($isDevelopment);
@@ -8,33 +7,20 @@
 ?>
 
 <?php
-	$merchandise = merchandiseReport($dbConn, $isDevelopment); 
+	$merchandises = merchandiseReport($dbConn); 
 	$template = $twig->load('merchandise.html');
+	echo $template->render(array('merchandises' => $merchandises));
 ?>
 
 <?php
-	function merchandiseReport($db, $isDevelopment) {
+	function merchandiseReport($db) {
 		$data = array();
-		if($isDevelopment) {
-			$results = pg_query($db ,"SELECT M.product, M.inventory, M.sold, M.units_sold_weekly, M.serial_number, M.s_id FROM merchandise as M;");
-			while($row = pg_fetch_row($results)) {
-				$productName = $row[0];
-				$inventory = $row[1];
-				$sold = $row[2];
-				$units_sold_weekly = $row[3];
-				$serial_number = $row[4];
-				$s_id = $row[5];
-				$data[] = array($productName, $inventory, $sold, $units_sold_weekly, $serial_number, $s_id);
-			}
-		} 
-		else {
-			$query = "SELECT M.product, M.inventory, M.sold, M.units_sold_weekly, M.serial_number, M.s_id FROM merchandise as M;";
-			$result = $db->query($query);
-			while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-				$data[] = array($row["product"], $row["inventory"], $row["sold"],$row["units_sold_weekly"], $row["serial_number"], $row[s_id], $isOperational);
-			}
-			$result->closeCursor();
-		}  
+		$query = "SELECT M.product, M.inventory, M.sold, M.units_sold_weekly, M.serial_number, M.s_id FROM merchandise as M;";
+		$result = $db->query($query);
+		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			$data[] = array($row["product"], $row["inventory"], $row["serial_number"], $row["s_id"]);
+		}
+		$result->closeCursor();
 		return $data;
 	}
 ?>
