8fa72c2a60c35658bc6654237b941d814282726e
mnnguy23 <michaelnguyen1105@gmail.com>
2017-04-06 12:01:07 -0500

    Added maintenanceReport.

---
+++ maintenanceReport.php
@@ -0,0 +1,82 @@
+<?php
+include 'app/base.php';
+include 'app/indexFunctions.php';
+$isDevelopment = false;
+$twig = loadEnvironment();
+$dbConn = loadDB($isDevelopment);
+?>
+
+
+<?php
+
+$template = $twig->load('maintenanceReport.html');
+$query = inputDateQuery($isDevelopment);
+
+$reports = maintenanceReport($dbConn, $isDevelopment, $query);
+$params = array('reports' => $reports);
+
+if($_SESSION['valid']){
+  echo $template->render($params);
+} else {
+  loginRedirect();
+}
+?>
+
+<?php
+function maintenanceReport($db, $isDevelopment, $query) {
+  $data = array();
+  if($isDevelopment) {
+    $results = pg_query($db ,$query);
+    while($row = pg_fetch_row($results)) {
+      $empName = $row[0];
+      $attractName = $row[1];
+      $maintDate = $row[2];
+      $cost = $row[3];
+      $data[] = array($empName, $attractName, $maintDate, $cost);
+    }
+  } else {
+    $query = "SELECT E.e_name, A.name, M.maintenance_date, M.maintenance_cost, A.operational FROM employee as E, attraction as A, attraction_maintenance as M WHERE M.e_id=E.employee_id and M.am_id=A.attraction_id;";
+    $result = $db->query($query);
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      if($row['operational'] == 't') {
+        $isOperational = 'Yes';
+      } else {
+        $isOperational = "No";
+      }
+      $data[] = array($row["e_name"], $row["name"], $row["maintenance_date"],$row["maintenance_cost"],  $isOperational);
+    }
+    $result->closeCursor();
+  } 
+  return $data;
+}
+?>
+
+
+<?php
+function inputDateQuery() {
+  if($isDevelopment) {
+    $query = "SELECT E.e_name, A.name, M.maintenance_date, M.maintenance_cost FROM public.employee AS E, public.attraction AS A, public.attraction_maintenance AS M WHERE M.e_id=E.employee_id AND M.am_id=A.attraction_id;";
+  
+    if(isset($_POST['submit']) && !empty($_POST["startDatepicker"]) && !empty($_POST["endDatepicker"])) {
+    
+      if($_POST["startDatepicker"] < $_POST["endDatepicker"]) {
+        $startDate = $_POST["startDatepicker"];
+        $endDate = $_POST["endDatepicker"];
+        $query = "SELECT E.e_name, A.name, M.maintenance_date, M.maintenance_cost FROM public.employee AS E, public.attraction AS A, public.attraction_maintenance AS M WHERE M.e_id=E.employee_id AND M.am_id=A.attraction_id AND M.maintenance_date between '$startDate' AND '$endDate';";
+      }
+    }
+  } else {
+    $query = "SELECT E.e_name, A.name, M.maintenance_date, M.maintenance_cost FROM employee AS E, attraction AS A, attraction_maintenance AS M WHERE M.e_id=E.employee_id AND M.am_id=A.attraction_id;";
+  
+    if(isset($_POST['submit']) && !empty($_POST["startDatepicker"]) && !empty($_POST["endDatepicker"])) {
+    
+      if($_POST["startDatepicker"] < $_POST["endDatepicker"]) {
+        $startDate = $_POST["startDatepicker"];
+        $endDate = $_POST["endDatepicker"];
+        $query = "SELECT E.e_name, A.name, M.maintenance_date, M.maintenance_cost FROM employee AS E, attraction AS A, attraction_maintenance AS M WHERE M.e_id=E.employee_id AND M.am_id=A.attraction_id AND M.maintenance_date between '$startDate' AND '$endDate';";
+      }
+    }
+  }
+  return $query;
+}
+?>
