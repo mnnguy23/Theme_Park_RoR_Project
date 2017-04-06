93d806e89a789b218216427e6f0b1145b302b8b9
jperez0198 <jperez0198@yahoo.com>
2017-04-06 15:16:53 -0500

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -25,4 +25,15 @@
 		$result->closeCursor();
 		return $data;
 	}
+	
+	function giftsReport($db) {
+		$data = array();
+		$query = "SELECT M.s_id, M.product, M.serial_number, M.inventory, E.m_date, E.units_sold FROM merchandise as M, merchandise_sales as E, shop as S WHERE S.service_type = 'gifts', M.s_id = S.shop_id;";
+		$result = $db->query($query);
+		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			$data[] = array($row["s_id"], $row["product"], $row["serial_number"], $row["inventory"], $row["m_date"], $row["units_sold"]);
+		}
+		$result->closeCursor();
+		return $data;
+	}
 ?>
