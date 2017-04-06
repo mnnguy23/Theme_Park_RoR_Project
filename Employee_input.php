def7759df9cc905bbd35eb24a108fc14e2d1350a
CPuerta <cfpuerta93@gmail.com>
2017-04-06 17:59:34 -0500

    Update Employee_input.php

--- Employee_input.php
+++ Employee_input.php
@@ -22,6 +22,28 @@
   $isDevelopment = false;
   $clearSession = developmentMode($isDevelopment);
 ?>
+<?php
+function loadDB($isDevelopment) {
+  if($isDevelopment) {
+    $host = "localhost";
+    $port = "5432";
+    $dbname = "theme_park";
+    $user = "tester";
+    $password = "1234";
+    $db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+  } else {
+    $dsn = "pgsql:"
+        . "host=ec2-50-17-236-15.compute-1.amazonaws.com;"
+        . "dbname=d2641hluvfmus5;"
+        . "user=nqxatreczgovme;"
+        . "port=5432;"
+        . "sslmode=require;"
+        . "password=91ef1bd79a01039737b248bdc7a465b27b284af93a537cfd217a5135d94f95d8";
+    $db = new PDO($dsn);
+  }
+  return $db;
+}
+?>
 
 <?php
 $template = $twig->load('addEmployee.html');
