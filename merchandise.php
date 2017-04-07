05bb1c650562698802e7d02630cbd158704bc175
jperez0198 <jperez0198@yahoo.com>
2017-04-07 18:39:09 -0500

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -8,12 +8,24 @@
 ?>
 
 <?php
+	$foods = foodReport($dbConn);
 	$gifts = giftReport($dbConn); 
 	$template = $twig->load('merchandise.html');
-	echo $template->render(array('gifts' => $gifts, 'logout' => $clearSession));
+	echo $template->render(array('foods' => $foods, 'gifts' => $gifts, 'logout' => $clearSession));
 ?>
 
 <?php
+	function giftReport($db) {
+		$data = array();
+		$query = "SELECT shop_id, product, serial_number, inventory FROM shop, merchandise WHERE shop_id = s_id AND service_type = 'food';";
+		$result = $db->query($query);
+		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			$data[] = array($row["shop_id"], $row["product"], $row["serial_number"], $row["inventory"]);
+		}
+		$result->closeCursor();
+		return $data;
+	}
+
 	function giftReport($db) {
 		$data = array();
 		$query = "SELECT shop_id, product, serial_number, inventory FROM shop, merchandise WHERE shop_id = s_id AND service_type = 'gifts';";
