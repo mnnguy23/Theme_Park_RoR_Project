77a09b9263d537a9fa1ec9d724afdccab1879be3
mnnguy23 <michaelnguyen1105@gmail.com>
2017-04-07 11:20:32 -0500

    select class still broken.

--- attractOperational.php
+++ attractOperational.php
@@ -16,6 +16,7 @@ function getAttractions($db, $isDevelopment) {
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
       $attract_id = $row['attraction_id'];
       $name = trim($row['name']);
+      echo "$name \n";
       $data[$attract_id] = $row[$name];
     
     }
