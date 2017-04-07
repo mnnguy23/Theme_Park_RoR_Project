eb4b1f043f57d20c37ea44f6be21bbe8380db981
jperez0198 <jperez0198@yahoo.com>
2017-04-07 00:38:09 -0500

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -16,7 +16,7 @@
 <?php
 	function merchandiseReport($db) {
 		$data = array();
-		$query = "SELECT M.s_id, M.product, M.serial_number, M.inventory FROM merchandise as M;";
+		$query = "SELECT M.s_id, M.product, M.serial_number, M.inventory FROM merchandise as M, shop as S WHERE M.s_id = S.shop_id AND S.service_type = 'food';";
 		$result = $db->query($query);
 		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
 			$data[] = array($row["s_id"], $row["product"], $row["serial_number"], $row["inventory"]);
