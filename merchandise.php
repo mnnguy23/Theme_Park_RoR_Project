d347604918167e8acd93a13bd7832e976191daa3
jperez0198 <jperez0198@yahoo.com>
2017-04-08 22:27:58 -0500

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -1,39 +1,112 @@
 <?php
 	include 'app/base.php';
 	include 'app/indexFunctions.php';
-	$isDevelopment = false;
 	$twig = loadEnvironment();
-	$clearSession = developmentMode($isDevelopment);
+	$isDevelopment = false;
 	$dbConn = loadDB($isDevelopment);
 ?>
 
 <?php
-	$foods = foodReport($dbConn);
-	$gifts = giftReport($dbConn); 
 	$template = $twig->load('merchandise.html');
-	echo $template->render(array('foods' => $foods, 'gifts' => $gifts, 'logout' => $clearSession));
+	$shops = shopReport($dbConn);
+	$msg = inputMerchandise($dbConn, $isDevelopment);
+	echo $template->render(array('shops' => $shops, 'msg' => $msg));
 ?>
 
 <?php
-	function foodReport($db) {
-		$data = array();
-		$query = "SELECT shop_id, product, name, serial_number, inventory FROM shop, merchandise WHERE shop_id = s_id AND service_type = 'food';";
-		$result = $db->query($query);
-		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			$data[] = array($row["shop_id"], $row["product"], $row["name"], $row["serial_number"], $row["inventory"]);
+	function inputMerchandise($db, $isDevelopment) {
+		$uniqueInfos = gatherInfo($db, $isDevelopment);
+  
+		$msg = "All fields must be entered.";
+		if(isset($_POST['submit'])) {
+			$name = $_POST["name"];
+     
+			if(!checkDuplicateProductName($uniqueInfos)) {
+				$product = $_POST["product"];
+			} 
+			else {
+				$msg = "Duplicate Product Name found";
+			}
+			
+			if(!checkSerialNumber($uniqueInfos)) {
+				$serial_number = $_POST["serial_number"];
+			} 
+			else {
+				$msg = "Duplicate Product Name found";
+			}
+		
+			$inventory = $_POST["inventory"];
+			$s_id = $_POST["s_id"];
+     
+			if(!checkDuplicateProductName($uniqueInfos) && !checkSerialNumber($uniqueInfos)){
+				$query = "INSERT INTO merchandise VALUES ('$product', $serial_number, $inventory, $s_id);";
+       
+				if($isDevelopment) {
+					$result = pg_query($db, $query);
+				} 
+				else {
+					$result = $db->query($query);
+				}
+       
+				if($result) {
+					$msg = "Merchandise: $name was successfully created.";
+				}
+			}
 		}
-		$result->closeCursor();
+    return $msg;
+	}
+?>
+
+<?php
+	function gatherInfo($db, $isDevelopment) {
+		$query = "SELECT * FROM merchandise;";
+		$data = array();
+  
+		if($isDevelopment) {
+			$result = pg_query($db, $query);
+			
+			while($row = pg_fetch_row($result)) {
+				$data[] = array('product' => $row[0], 'serial_number' => $row[1], 'inventory' => $row[2], 's_id' => $row[3]);
+			}
+		} 
+		else {
+			$result = $db->query($query);
+    
+			while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+				$data[] = array('product' => $row['product'], 'serial_number' => $row['serial_number'], 'inventory' => $row["inventory"], "s_id" => $row["s_id"]);
+			}
+		}
+  
 		return $data;
 	}
+?>
 
-	function giftReport($db) {
-		$data = array();
-		$query = "SELECT shop_id, product, name, serial_number, inventory FROM shop, merchandise WHERE shop_id = s_id AND service_type = 'gifts';";
-		$result = $db->query($query);
-		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			$data[] = array($row["shop_id"], $row["product"], $row["name"], $row["serial_number"], $row["inventory"]);
-		}
-		$result->closeCursor();
-		return $data;
+<?php
+	function checkDuplicateProductName($infos) {
+		$result = false;
+			
+			foreach($infos as $info) {
+				$product = $info['product'] ?? null;
+			
+				if($product == $_POST["product"]){
+					$result = true;
+				} 
+			}
+		return $result;
+	}
+?>
+
+<?php
+	function checkSerialNumber($infos) {
+		$result = false;
+			
+			foreach($infos as $info) {
+				$serial_number = $info['serial_number'] ?? null;
+			
+				if($serial_number == $_POST["serial_number"]){
+					$result = true;
+				} 
+			}
+		return $result;
 	}
 ?>
