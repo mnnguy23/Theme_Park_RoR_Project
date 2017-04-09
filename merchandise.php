9634de1dc84c9570d66be736d185964404f14dcc
jperez0198 <jperez0198@yahoo.com>
2017-04-08 22:51:22 -0500

    Update merchandise.php

--- merchandise.php
+++ merchandise.php
@@ -8,37 +8,36 @@
 
 <?php
 	$template = $twig->load('merchandise.html');
-	$msg = inputMerchandise($dbConn, $isDevelopment);
+	$msg = inputEmployee($dbConn, $isDevelopment);
 	echo $template->render(array('msg' => $msg));
 ?>
 
 <?php
-	function inputMerchandise($db, $isDevelopment) {
+	function inputEmployee($db, $isDevelopment) {
 		$uniqueInfos = gatherInfo($db, $isDevelopment);
-  
 		$msg = "All fields must be entered.";
+		
 		if(isset($_POST['submit'])) {
-			$name = $_POST["name"];
-     
-			if(!checkDuplicateProductName($uniqueInfos)) {
+			if(!checkDuplicateProduct($uniqueInfos)) {
 				$product = $_POST["product"];
 			} 
 			else {
 				$msg = "Duplicate Product Name found";
 			}
+     
+			$inventory = $_POST["inventory"];
 			
-			if(!checkSerialNumber($uniqueInfos)) {
+			if(!checkDuplicateSerialNumber($uniqueInfos)) {
 				$serial_number = $_POST["serial_number"];
 			} 
 			else {
-				$msg = "Duplicate Product Name found";
+				$msg = "Duplicate Serial Number Found";
 			}
-		
-			$inventory = $_POST["inventory"];
+			
 			$s_id = $_POST["s_id"];
      
-			if(!checkDuplicateProductName($uniqueInfos) && !checkSerialNumber($uniqueInfos)){
-				$query = "INSERT INTO merchandise VALUES ('$product', $serial_number, $inventory, $s_id);";
+			if(!checkDuplicateProduct($uniqueInfos) && !checkDuplicateSerialNumber($uniqueInfos)){
+				$query = "INSERT INTO merchandise VALUES ('$product', $inventory, $serial_number, $s_id);";
        
 				if($isDevelopment) {
 					$result = pg_query($db, $query);
@@ -46,13 +45,13 @@
 				else {
 					$result = $db->query($query);
 				}
-       
+		
 				if($result) {
-					$msg = "Merchandise: $name was successfully created.";
+					$msg = "Merchandise: $product was successfully created.";
 				}
 			}
-		}
-    return $msg;
+		} 
+		return $msg;
 	}
 ?>
 
@@ -63,25 +62,24 @@
   
 		if($isDevelopment) {
 			$result = pg_query($db, $query);
-			
+    
 			while($row = pg_fetch_row($result)) {
-				$data[] = array('product' => $row[0], 'serial_number' => $row[1], 'inventory' => $row[2], 's_id' => $row[3]);
+				$data[] = array('product' => $row[0], 'inventory' => $row[1], 'serial_number' => $row[2], 's_id' => $row[3]);
 			}
 		} 
 		else {
 			$result = $db->query($query);
     
 			while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-				$data[] = array('product' => $row['product'], 'serial_number' => $row['serial_number'], 'inventory' => $row["inventory"], "s_id" => $row["s_id"]);
+				$data[] = array('product' => $row['product'], 'inventory' => $row['inventory'], 'serial_number' => $row["serial_number"], "s_id" => $row["s_id"]);
 			}
 		}
-  
 		return $data;
 	}
 ?>
 
 <?php
-	function checkDuplicateProductName($infos) {
+	function checkDuplicateProduct($infos) {
 		$result = false;
 
 		foreach($infos as $info) {
@@ -96,16 +94,16 @@
 ?>
 
 <?php
-	function checkSerialNumber($infos) {
+	function checkDuplicateSerialNumber($infos) {
 		$result = false;
-			
-			foreach($infos as $info) {
-				$serial_number = $info['serial_number'] ?? null;
-			
-				if($serial_number == $_POST["serial_number"]){
-					$result = true;
-				} 
-			}
+
+		foreach($infos as $info) {
+			$serial_number = $info['serial_number'] ?? null;
+    
+			if($serial_number == $_POST["serial_number"]){
+				$result = true;
+			} 
+		}
 		return $result;
 	}
 ?>
