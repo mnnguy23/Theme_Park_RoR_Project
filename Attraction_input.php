3fbf29740be208e09a5b16de9271d3d2af66b951
CPuerta <cfpuerta93@gmail.com>
2017-04-09 00:44:26 -0500

    Update Attraction_input.php

--- Attraction_input.php
+++ Attraction_input.php
@@ -38,7 +38,7 @@ function inputEmployee($db, $isDevelopment) {
 	   
      
      if(!checkDuplicateAname($uniqueInfos) ){
-       $query = "INSERT INTO employee VALUES ($att_id, $price, $capacity, '$date_built', '$m_date', '$name', $dno, $op_cost,DEFAULT);";
+       $query = "INSERT INTO employee VALUES ($att_id, $price, $capacity, '$date_built', '$m_date', '$name', $dno, $op_cost,true);";
        
        if($isDevelopment) {
          $result = pg_query($db, $query);
