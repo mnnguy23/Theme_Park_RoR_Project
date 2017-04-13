d21f1a417192f6a93866d736114b7216288896a2
CPuerta <cfpuerta93@gmail.com>
2017-04-13 12:35:49 -0500

    Update Attraction_input.php

--- Attraction_input.php
+++ Attraction_input.php
@@ -36,7 +36,7 @@ function inputEmployee($db, $isDevelopment) {
      //all inputs have been tested for correctnes with $msg	   
      
      if(!checkDuplicateAname($uniqueInfos) ){
-       $query = "INSERT INTO attraction VALUES ($att_id, $price, $capacity, '$date_built', '$m_date', '$name',  TRUE,NULL);";
+       $query = "INSERT INTO attraction VALUES ($att_id, $price, $capacity, '$date_built', '$m_date', '$name',  TRUE);";
        
        if($isDevelopment) {
          $result = pg_query($db, $query);
