79d32d7c6b4447a5f52fcd79341d29064e08f6ce
jperez0198 <jperez0198@yahoo.com>
2017-04-13 15:18:46 -0500

    Update changePassword.php

--- changePassword.php
+++ changePassword.php
@@ -1,44 +1,70 @@
 <?php
 	include 'app/base.php';
 	include 'app/indexFunctions.php';
-  $twig = loadEnvironment();
-  $isDevelopment = false;
-  $dbConn = loadDB($isDevelopment);
+	$twig = loadEnvironment();
+	$isDevelopment = false;
+	$dbConn = loadDB($isDevelopment);
 ?>
 
 <?php
-$template = $twig->load('changePassword.html');
-$msg = inputEmployee($dbConn, $isDevelopment);
-if($_SESSION['valid']){
-  echo $template->render(array('msg' => $msg, 'dno' => $_SESSION['dno']));
-} else {
-  loginRedirect();
-}
+	$template = $twig->load('changePassword.html');
+	$msg = changePassword($dbConn, $isDevelopment);
+
+	if(!$_SESSION['isManager']) {
+		menuRedirect();
+	}
+	
+	if($_SESSION['valid']){
+		echo $template->render(array('msg' => $msg, 'dno' => $_SESSION['dno']));
+	} 
+	else {
+		loginRedirect();
+	}
 ?>
 
 <?php
-function inputEmployee($db, $isDevelopment) {
-     $oldpass = $_POST["old_password"];
-     $newpass = $_POST["new_password"];
-     $cpass = $_POST["confirm_password"];
-  
-  $msg = "All fields must be entered.";
-   if(isset($_POST['submit'])) {
-     $name = $_POST["attraction_name"];
+	function changePassword($db, $isDevelopment) {
+		$oldPassword = $_POST["old_password"];
+		$newPassword = $_POST["new_password"];
+		$confirmPassword = $_POST["confirm_password"];
+		$msg = "All fields must be entered.";
+
+		if(isset($_POST['submit'])) {
+			$name = $_POST["attraction_name"];
      
-     if(!checkDuplicatePassword($newpass, $cpass)) {
-       $name = $_POST["old_password"];
-	//$msg="Your password has been changed";     
-     } else {
-       $msg = "The passwords don't match name found";
-     }
+			if(!checkDuplicatePassword($newPassword, $confirmPassword)) {
+				$name = $_POST["old_password"];  
+			} 
+			else {
+				$msg = "Wrong password";
+			}  
+     
+			if(!checkDuplicateAname($uniqueInfos) ){
+				$query = "UPDATE employee SET password = $newPassword WHERE employee_password = $oldPassword;";
+       
+				if($isDevelopment) {
+					$result = pg_query($db, $query);
+				} 
+				else {
+					$result = $db->query($query);
+				}
+		
+				if($result) {
+					$msg = "Change Password: $Your passord was changed.";
+				}
+			}
+		}
+		return $msg;
+	}
+?>
 
 <?php
-function checkDuplicatePassword($newpass, $cpass) {
-  $result = false;
-    if($newpass == $cpass){
-      $result = true;
-    } 
-  return $result;
-}
+	function checkDuplicatePassword($newPassword, $confirmPassword) {
+		$result = false;
+    
+		if($newPassword == $confirmPassword){
+			$result = true;
+		} 
+		return $result;
+	}
 ?>
